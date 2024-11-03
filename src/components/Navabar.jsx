54d68f75b99545e238a787073f66932150a877b3
Akash Yeole <akashyeole02@gmail.com>
2024-11-03 23:30:42 +0530

    add navbar links

--- src/components/Navabar.jsx
+++ src/components/Navabar.jsx
@@ -1,11 +1,32 @@
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarItem, Link} from "@nextui-org/react";
+import { Link as LN, useLocation } from "react-router-dom";
+import { ArrowLeftIcon, CheckIcon } from "@nextui-org/shared-icons";
 
 function TodoNavbar() {
+  const location = useLocation();
   return (
-    <Navbar>
-      <NavbarBrand className="f-[100%] flex flex-row items-center justify-center">
-        <p className="font-bold text-inherit">Todo App</p>
-      </NavbarBrand>
+    <Navbar className="flex flex-row">
+        <NavbarBrand className="w-fit">
+          <LN to={'/'}>
+            <p className="font-bold text-inherit">Todo App</p>
+          </LN>
+        </NavbarBrand>
+        <NavbarItem  className="w-fit">
+            {location.pathname === '/completed' ? 
+                <LN to={'/'}>
+                  <p className="flex flex-row items-center gap-2 text-blue-500">
+                    Incomplete todos
+                    <ArrowLeftIcon/>
+                  </p>
+                </LN>: 
+                <LN to={'/completed'}>
+                  <p className="flex flex-row items-center gap-2 text-green-500">
+                    Completed todos
+                    <CheckIcon/>
+                  </p>
+                </LN>
+            }
+        </NavbarItem>
     </Navbar>
   );
 }
